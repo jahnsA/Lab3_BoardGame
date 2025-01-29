77d829c4d002397ce554cb87d680b26562b43760
Abby Jahns <2abbylouwho@gmail.com>
2025-01-29 08:59:17 -0800

    cleaned up card methods, still need to fix constructor making new deck

--- BoardGame.java
+++ BoardGame.java
@@ -1,19 +1,30 @@
 import java.security.SecureRandom;
-//import javax.smartcardio.Card;
 import java.util.Stack;
 
 public class BoardGame {
     private static final SecureRandom randomNumbers = new SecureRandom();
     private static final int NUMBER_OF_CARDS = 45;
+    private Stack<Card> Deck;
 
     //creates an array of Card objects (deck)
     private Card[] deck = new Card[NUMBER_OF_CARDS]; //Card references
+    //create a stack for deck of cards
+    Stack<Card> stackedDeck = new Stack<>();
+
+    // Constructor fills deck of cards
+    public BoardGame() {
+        // Populate deck with Card objects using card type enums
+        for (int count = 0; count < deck.length; count++) {
+            deck[count] = new Card(cardType.value);
+            //deck[count] = new Card(Face.values()[count % 13], Suit.values()[count / 13]);
+        }//end for loop
+    }//end DeckOfCards constructor
 
     //shuffle deck of Cards with one-pass algorithm
     public void shuffle() {
         //next call to method dealCard should start at deck[0] again
         for (int first = 0; first < deck.length; first++) {
-            //select a random number between 0 and 51
+            //select a random number between 0 and 45
             int second = randomNumbers.nextInt(deck.length);
 
             //swap current Card with randomly selected Card
@@ -23,16 +34,13 @@ public class BoardGame {
         }//end for loop
     }//end of shuffle method
 
-        //shuffle array and turn to stack
-    public void stackDeck(Stack<Card> stackedDeck){
-        //shuffle deck until last card in array is not an 8 (so that first card
-        //dealt into discard pile will not be 8)
-        do { 
-            shuffle();
-        } while(deck[deck.length - 1].getFace() == Face.EIGHT);
+    //shuffle array and turn to stack
+    public void stackDeck(){
+        //sets up game by shuffling deck
+        shuffle();
         //add array cards to deck 
         for (int i = 0; i < deck.length; i++) {
-            stackedDeck.push(deck[i]);
+            this.Deck.push(deck[i]);
         }//end for loop
     }//end stackDeck
 
