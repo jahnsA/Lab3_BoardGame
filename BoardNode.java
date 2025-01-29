47c0f71192f9aa03af1576d1978b904efe90ab05
sarsparvier <sarsparvier@gmail.com>
2025-01-29 13:38:04 -0800

    created boardNode class

--- BoardNode.java
+++ BoardNode.java
@@ -1,1 +1,21 @@
 
+enum nodePosition {
+    spawnPoint, homeSpot, sliderStartSpot, regularSpot
+} 
+public class BoardNode {
+    nodePosition position;
+    BoardNode next;
+    BoardNode prev;
+    
+    public BoardNode(nodePosition position) {
+        //data = x;
+        this.position = position;
+        next = null; //points to node in front
+        prev = null; //points to node behind
+    }
+
+    public void setNodePosition(nodePosition position) {
+        this.position = position;
+    }
+
+}
