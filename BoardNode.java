9f58471a5819a1c49d6926ec945437ff8fc191f5
sarsparvier <sarsparvier@gmail.com>
2025-01-31 08:39:54 -0800

    added a getNodePosition method for boardNode class

--- BoardNode.java
+++ BoardNode.java
@@ -8,14 +8,21 @@ public class BoardNode {
     BoardNode prev;
     
     public BoardNode(nodePosition position) {
-        //data = x;
         this.position = position;
         next = null; //points to node in front
         prev = null; //points to node behind
     }
 
+    //return what the node position is
+    public nodePosition getNodePosition() {
+        return this.position;
+    }
+
+    //set the node position to another thing
     public void setNodePosition(nodePosition position) {
         this.position = position;
     }
+
+    
 
 }
---
+++ SarahMethod.java
@@ -0,0 +1,13 @@
+public  class SarahMethod {
+
+        //a slider method
+        //where if they hit the spot that has slider spot, then move forward five spaces
+    public void slider(BoardNode node) {
+        //
+        if (node.getNodePosition() == nodePosition.sliderStartSpot) {
+            //
+        }
+
+    }
+
+}
