3e1d1bccf4c5146ac34851d53ee1aff4c5dbf463
lizeyliz <alizaeaton@gmail.com>
2025-01-31 09:45:05 -0800

    working on ideas for board

--- Main.java
+++ Main.java
@@ -5,5 +5,22 @@ public class Main {
     public static void main(String[] args) {
         BoardGame boardGame = new BoardGame();
         boardGame.createBoard();
+/*
+|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|
+|_| |_|                       |_|
+|_| |_|                       |_|
+|_| |_|                       |_|
+|_| |_|                       |_|
+|_| |_|                       |_|
+|_| |* *|                     |_|
+|_| |* *|                     |_|
+|_|                     |   | |_|
+|_|                     |   | |_|
+|_|                       |_| |_|
+|_|                       |_| |_|
+|_|                       |_| |_|
+|_|                       |_| |_|
+|_|                       |_| |_|
+|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|  */ 
     }//end main method
 }//end class Main
